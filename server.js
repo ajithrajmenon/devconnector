754a5bd9a434d3136abe85ecdf6d712b9f1ac27a
ajithrajmenon <ajith.raj.menon@gmail.com>
2018-07-21 03:02:34 +0530

    User registration logic

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -7,8 +8,13 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const db = require("./config/keys").mongoURI;
 
+// Connect to mongo
 mongoose
   .connect(db)
   .then(() => console.log("MongoDB connected"))
